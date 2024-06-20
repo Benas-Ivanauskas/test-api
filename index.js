408d642626655b6a5f899a0da542965c4fdf90ef
Benas-Ivanauskas <benas.ivanauskas@gmail.com>
2024-06-20 14:42:24 +0300

    asd

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const jsonServer = require("json-server");
+const path = require("path");
+const express = require("express");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = 3001;
+const port = process.env.PORT || 3001;
+
+// Serve static files (images)
+server.use("/images", express.static(path.join(__dirname, "images")));
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
